eb6b6f621b8b0317b451168cb7525124d94b1587
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-07-03 16:19:29 +0200

    light/dark switcher working

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,15 +1,30 @@
 import { Text, ScrollView} from 'react-native';
 import Selector from '@/components/Selector';
+import { Appearance } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function TabTwoScreen() {
+  const [appTheme, setAppTheme] = useState<number>();
+
+  useEffect(() => {
+    switch (appTheme) {
+      case 0:
+        Appearance.setColorScheme(null);
+        break;
+      case 1:
+        Appearance.setColorScheme('light');
+        break;
+      case 2:
+        Appearance.setColorScheme('dark');
+        break;
+    }
+  }, [appTheme]);
 
   return (
     <ScrollView className='h-screen w-full py-16 px-8'>
       <Text className='text-center text-2xl dark:text-stone-100 font-bold p-8 pb-4'>Settings</Text>
-      <Selector title='Theme' label1='Automatic' label2='Light' label3='Dark' />
-      <Selector title='Theme' label1='Automatic' label2='Light' label3='Dark' />
-      <Selector title='Theme' label1='Automatic' label2='Light' label3='Dark' />
-      <Selector title='Theme' label1='Automatic' label2='Light' label3='Dark' />
+      <Selector title='Language' label1='English' label2='Deutsch' />
+      <Selector title='Theme' label1='Automatic' label2='Light' label3='Dark' state={setAppTheme} />
     </ScrollView>
   );
 }
--- components/Selector.tsx
+++ components/Selector.tsx
@@ -1,31 +1,41 @@
-import React, { useState } from 'react';
+import React, { SetStateAction, useState } from 'react';
 import { Text, View, Switch } from 'react-native';
 
 interface SelectorProps {
     title: string;
     label1: string;
     label2: string;
-    label3: string;
+    label3?: string;
+    state?: React.Dispatch<React.SetStateAction<any>>;
 }
 
-const Selector = ({title, label1, label2, label3}: SelectorProps) => {
+const Selector = ({ title, label1, label2, label3, state }: SelectorProps) => {
     const [switch1Value, setSwitch1Value] = useState(false);
     const [switch2Value, setSwitch2Value] = useState(false);
     const [switch3Value, setSwitch3Value] = useState(false);
 
     const handleSwitch1Change = () => {
+        if (state) {
+            state(0);
+        }
         setSwitch1Value(true);
         setSwitch2Value(false);
         setSwitch3Value(false);
     };
 
     const handleSwitch2Change = () => {
+        if (state) {
+            state(1);
+        }
         setSwitch1Value(false);
         setSwitch2Value(true);
         setSwitch3Value(false);
     };
 
     const handleSwitch3Change = () => {
+        if (state) {
+            state(2);
+        }
         setSwitch1Value(false);
         setSwitch2Value(false);
         setSwitch3Value(true);
@@ -34,21 +44,25 @@ const Selector = ({title, label1, label2, label3}: SelectorProps) => {
     return (
         <View className='flex flex-col gap-2 mb-8'>
             <Text className='text-sm text-left px-4 text-stone-500'>{title}</Text>
-            <View className='flex flex-col gap-2 p-2 bg-white rounded-lg'>
-                <View className='flex flex-row justify-between items-center'>
+            <View className='flex flex-col px-4 bg-white rounded-lg'>
+                <View className='flex flex-row py-2 justify-between items-center'>
                     <Text className='dark:text-stone-400'>{label1}</Text>
                     <Switch value={switch1Value} onValueChange={handleSwitch1Change} />
                 </View>
                 <View className="w-full h-px bg-gray-300 rounded-full mx-auto" />
-                <View className='flex flex-row justify-between items-center'>
+                <View className='flex flex-row py-2 justify-between items-center'>
                     <Text className='dark:text-stone-400'>{label2}</Text>
                     <Switch value={switch2Value} onValueChange={handleSwitch2Change} />
                 </View>
-                <View className="w-full h-px bg-gray-300 rounded-full mx-auto" />
-                <View className='flex flex-row justify-between items-center'>
-                    <Text className='dark:text-stone-400'>{label3}</Text>
-                    <Switch value={switch3Value} onValueChange={handleSwitch3Change} />
-                </View>
+                {label3 && (
+                    <>
+                        <View className="w-full h-px bg-gray-300 rounded-full mx-auto" />
+                        <View className='flex flex-row py-2 justify-between items-center'>
+                            <Text className='dark:text-stone-400'>{label3}</Text>
+                            <Switch value={switch3Value} onValueChange={handleSwitch3Change} />
+                        </View>
+                    </>
+                )}
             </View>
         </View>
     );
